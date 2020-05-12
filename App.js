704433eedfc76d8cfc75ba031cae72a2228e5bf1
analiseburtet <analiseburtet@gmail.com>
2020-05-12 01:29:24 -0300

    adicionar UI do componente account balance

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header'
+import AccountAmount from './src/components/AccountAmount'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header/>
+      <AccountAmount/>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
