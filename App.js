9d575c4a9356f0aa55e07090129cacce69af879c
analiseburtet <analiseburtet@gmail.com>
2020-05-12 01:05:31 -0300

    iniciar app e adicionar header

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header/>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
@@ -12,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F2F1F0',
+    flexDirection: 'column'
   },
 });
---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+
+const Header = () => {
+    return(
+        <View style={styles.background}>
+            <Image
+                style={styles.logo}
+                source={require('../../assets/logo.png')}
+            />
+            <Text
+                 style={styles.text}
+            >Piggy Bank</Text>
+        </View>
+    )
+}
+
+export default Header
+
+const styles =  StyleSheet.create({
+    background: {
+        backgroundColor: '#77216F',
+        flex: 0.1,
+        padding: 14,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    },
+    logo: {
+        height: 64,
+        width: 64
+    },
+    text: {
+        fontSize: 24,
+        fontFamily: 'sans-serif',
+        color: '#111111',
+        fontWeight: 900,
+        paddingLeft: 10
+    }
+})
